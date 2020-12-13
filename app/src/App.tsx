73ae21f33223ca9e480b48be174c9e6b0bc0c3d7
jkaplanis <johntkaplanis@gmail.com>
2020-12-12 16:03:28 -0800

    removed unused imports from App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { useSelector } from "react-redux";
-import { RootState } from "./root-reducer";
-import { Route, BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import './App.css';
 import { Provider } from 'react-redux';
 import { store } from './store';
